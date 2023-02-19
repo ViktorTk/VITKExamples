2ba8d78dfeb0090cb5d84ddf9942ef9e19dd38a4
Виктор <viktor.avgn@yandex.ru>
2023-02-19 21:01:04 +0700

    Exmpl5-Особое приветствие пользователя

---
+++ Examples/Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "эля")
+{
+    Console.WriteLine("Ура! Это же ЭЛЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
