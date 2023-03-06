096f627867c66ea243c5b8a3c8645064317a8cc2
Виктор <viktor.avgn@yandex.ru>
2023-03-06 15:30:14 +0700

    Exmp10

---
+++ Examples/Example10/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int [] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};  //если есть индексы с одинаковыми значениями -> покажет все значения
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если выставить брейк - будет показывать только первый искомый элемент (дальнейшие элементы не будет выводить)
+    }
+    //index = index + 1
+    index++;
+}
